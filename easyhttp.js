9bbd5df8dd03ba0f2722c352cddb67d6e9236a26
harshbutfair <harsh.narayan@yahoo.com>
2021-06-29 22:43:30 +0530

    added POST PUT DELETE request

--- easyhttp.js
+++ easyhttp.js
@@ -2,7 +2,7 @@ function easyHTTP() {
     this.http = new XMLHttpRequest();
 }
 
-// Make an HTTP GET Reuest
+// Make an HTTP GET Request
 easyHTTP.prototype.get = function(url, callback) {
     this.http.open('GET', url, true);
 
@@ -18,10 +18,46 @@ easyHTTP.prototype.get = function(url, callback) {
     this.http.send();
 }
 
-// Make an HTTP POST Reuest
+// Make an HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
 
 
-// Make an HTTP PUT Reuest
+    this.http.send(JSON.stringify(data));
+}
+
+// Make an HTTP PUT Request
+easyHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
 
 
-// Make an HTTP DELETE Reuest
+    this.http.send(JSON.stringify(data));
+}
+
+// Make an HTTP DELETE Request
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if(self.http.status === 200){
+            callback(null, 'Post Deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
